d241a2afdf7f77bd12c71f1ea3601f67bdd82269
liyyf <liyyf@tuya.com>
2024-05-06 09:57:46 +0800

    feat(common): :tada: 初始化

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react'
+import './index.css'
+
+function Pages() {
+  // const dpsRef = useRef<any>()
+  // const [zengyiVisible, setZengyiVisible] = useState<boolean>(true)
+
+  // const getDps = () => {
+  //   dpsRef?.current?.getDps()
+  // }
+
+  return (
+    <>
+      {/* 顶部说明 */}
+      {/* <TitleTip /> */}
+      {/* 角色设置 */}
+      {/* <BasicSet getDps={getDps} zengyiVisible={zengyiVisible} setZengyiVisible={setZengyiVisible} /> */}
+      {/* Dps计算 */}
+      {/* <Dps zengyiVisible={zengyiVisible} ref={dpsRef} /> */}
+      {/* 更新日志 */}
+      {/* <Log /> */}
+      {/* 开发者工具 */}
+      {/* <Tools /> */}
+      {/* 清除缓存 */}
+      {/* <ClearCache /> */}
+    </>
+  )
+}
+
+export default Pages
