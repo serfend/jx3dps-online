21236c060ce4c83147dae774caba0c9425abe635
liyyf <liyyf@tuya.com>
2024-05-15 16:42:20 +0800

    feat(蓬莱): :sparkles: 增加蓬莱的快捷增益设置

--- src/心法模块/心法/凌海诀/默认数据/index.ts
+++ src/心法模块/心法/凌海诀/默认数据/index.ts
@@ -1,9 +1,9 @@
 import 默认秘籍 from './秘籍.json'
 import 默认配装 from './配装'
-// import 增益快捷设置数据 from './增益快捷设置数据'
+import 增益快捷设置数据 from './增益快捷设置数据'
 
 export default {
   配装: 默认配装,
   秘籍: 默认秘籍,
-  // 增益快捷设置数据,
+  增益快捷设置数据,
 }
