94cc031bb33c8a8cf6b8f503e6e510ef30733f01
liyyf <liyyf@tuya.com>
2024-05-27 12:35:15 +0800

    fix: 木桩大赛

--- src/心法模块/心法/太玄经/计算循环/index.ts
+++ src/心法模块/心法/太玄经/计算循环/index.ts
@@ -6,12 +6,56 @@ import type { 循环数据 } from '@/@types/循环'
  */
 
 // 循环
-import 堪炸 from './堪炸.json'
-import 鬼列 from './鬼列.json'
-import 橙武 from './橙武.json'
-// import 鬼列_test from './鬼列_test.json'
-// import 橙武加速测试 from './橙武加速测试.json'
+import 奶茶鉴定专家_1 from './参赛作品/紫武/奶茶鉴定专家_1.json'
+import 奶茶鉴定专家_2 from './参赛作品/紫武/奶茶鉴定专家_2.json'
+import 小满 from './参赛作品/紫武/小满.json'
+import 神算子_1 from './参赛作品/紫武/神算子_1.json'
+import 神算子_2 from './参赛作品/紫武/神算子_2.json'
+import 心如刀_1 from './参赛作品/橙武/心如刀_1.json'
+import 心如刀_2 from './参赛作品/橙武/心如刀_2.json'
+import 心如刀_3 from './参赛作品/橙武/心如刀_3.json'
+import 神算子_cw_1 from './参赛作品/橙武/神算子_1.json'
+import 神算子_cw_2 from './参赛作品/橙武/神算子_2.json'
+import 纸狐 from './参赛作品/橙武/纸狐.json'
+import 臭猪 from './参赛作品/橙武/臭猪.json'
+import 镇星入舆 from './参赛作品/橙武/镇星入舆.json'
+import 风雪 from './参赛作品/紫武/风雪.json'
+import 江岸停 from './参赛作品/紫武/江岸停.json'
 
-const 计算循环: 循环数据[] = [鬼列, 堪炸, 橙武] as 循环数据[]
+const 计算循环: 循环数据[] = [
+  奶茶鉴定专家_1,
+  奶茶鉴定专家_2,
+  小满,
+  神算子_1,
+  神算子_2,
+  风雪,
+  江岸停,
+  // 心如刀_1,
+  // 心如刀_2,
+  // 心如刀_3,
+  // 神算子_cw_1,
+  // 神算子_cw_2,
+  // 纸狐,
+  // 臭猪,
+  // 镇星入舆,
+] as 循环数据[]
+
+const 计算循环2: 循环数据[] = [
+  // 奶茶鉴定专家_1,
+  // 奶茶鉴定专家_2,
+  // 小满,
+  // 神算子_1,
+  // 神算子_2,
+  心如刀_1,
+  心如刀_2,
+  心如刀_3,
+  神算子_cw_1,
+  神算子_cw_2,
+  纸狐,
+  臭猪,
+  镇星入舆,
+] as 循环数据[]
+
+console.log('计算循环2', 计算循环2)
 
 export default 计算循环
