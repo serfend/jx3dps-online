f8855065a4987abaa1b84f4202a7a5d7ad75447b
liyyf <liyyf@tuya.com>
2024-07-17 16:38:58 +0800

    feat(版本发布): :rocket: [雾海寻龙] 1.1.11 优化增益算法

--- src/心法模块/心法/太玄经/计算循环/index.ts
+++ src/心法模块/心法/太玄经/计算循环/index.ts
@@ -10,7 +10,7 @@ import 堪炸 from './堪炸.json'
 import 鬼追 from './鬼追.json'
 import 鬼列 from './鬼列.json'
 import 橙武 from './橙武.json'
-// import 鬼列_test from './鬼列_test.json'
+// import 测试循环 from './测试循环.json'
 // import 橙武加速测试 from './橙武加速测试.json'
 
 const 计算循环: 循环数据[] = [鬼列, 堪炸, 鬼追, 橙武] as 循环数据[]
